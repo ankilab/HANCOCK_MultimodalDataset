5ff31699c877807a44119115883a2ebb7260111d
dj-fau <david.julian.fischer@fau.de>
2024-07-12 09:09:10 +0200

    Added documentation to abstract TabularAdjuvantTreatmentModelEvaulator

--- model_evaluation/tabular_adjuvant_therapy.py
+++ model_evaluation/tabular_adjuvant_therapy.py
@@ -10,9 +10,21 @@ from multimodal_machine_learning import TabularAdjuvantTreatmentPredictor
 
 
 class TabularAdjuvantTreatmentModelEvaluator:
+    """For Evaluating Hyper-Parameters of Adjuvant Treatment Models
+    on the merged tabular (multi-modal) data set.
+    """
     def __init__(
             self, plot_flag: bool = False, save_flag: bool = False, random_state: int = 42
-    ):
+    ) -> None:
+        """Creates an instance of the TabularAdjuvantTreatmentModelEvaluator. Should not be used
+        directly because this is intended to be an abstract class.
+
+        Args:
+            plot_flag (bool, optional): Flag to plot the results. Defaults to False.
+            save_flag (bool, optional): Flag to save the results. Defaults to False
+            random_state (int, optional): Random state seed for reproducibility. Will be passed to
+            all functions classes that contain random events. Defaults to 42.
+        """
         self.predictor = TabularAdjuvantTreatmentPredictor(
             plot_flag=plot_flag, save_flag=save_flag, random_state=random_state
         )
@@ -29,6 +41,42 @@ class TabularAdjuvantTreatmentModelEvaluator:
             self, n_splits: int = 10, n_iter: int = 100, scoring: str = 'f1',
             plot_name: str = 'random_search_adjuvant_therapy'
     ) -> dict[str, any]:
+        """Performs a random search with cross-validation on the model
+        (call of _create_base_model()) using the hyper-parameter space
+        defined (call of create_hyper_parameter_space_random()).
+
+        Args:
+            n_splits (int, optional): Number of splits for cross-validation.
+            Defaults to 10.
+
+            n_iter (int, optional): Number of iterations for the random search.
+            Will choose n_iter hyperparameter combinations. Defaults to 100.
+
+            scoring (str, optional): Scoring metric for the selection of the best model.
+            For more metrics check
+            https://scikit-learn.org/stable/modules/model_evaluation.html#scoring-parameter.
+            Defaults to 'f1'.
+
+            plot_name (str, optional): Name of the plot (also used for saving the plot).
+            Defaults to 'random_search_adjuvant_therapy'.
+
+        Returns:
+            dict[str, any]: Returns a dictionary with the key 'train_returns' and the value
+            of the training returns of the model.
+            The train_returns are:
+            list: A list with the validation parameters and the data that was used.
+            [[fpr, tpr, auc], [x_train, y_train, x_other, y_other, y_pred], features]
+            fpr (np.array): False positive rate
+            tpr (np.array): True positive rate
+            auc (float): The AUC score
+            x_train (np.ndarray): The training data encoded
+            y_train (np.array): The training labels
+            x_other (np.ndarray): The validation data encoded
+            y_other (np.array): The validation labels
+            y_pred (np.array): The predicted labels for the validation data
+            features (list): The features from the ColumnTransformer that was used for
+            encoding the training data.
+        """
         base_model = self._create_base_model()
         random_search_predictor = RandomizedSearchCV(
             param_distributions=self.hyper_parameter_space_random,
@@ -42,25 +90,48 @@ class TabularAdjuvantTreatmentModelEvaluator:
         self._random_search_performed = True
         self._grid_search_performed = False
         if self._save_flag:
-            self.save_random_search_results(file_name=plot_name)
+            self._save_random_search_results(file_name=plot_name)
         return {'train_returns': train_returns}
 
     def get_best_random_search_model(self) -> any:
+        """If random search was performed and no grid search inbetween,
+        the best estimator will be returned from the random search.
+
+        Returns:
+            any: None of no random search was performed, else the best
+            estimator.
+        """
         if not self._random_search_performed:
             print('Random search not performed!')
             return None
         return self.predictor.model.best_estimator_
 
     def get_best_random_search_parameters(self) -> dict | None:
+        """If random search was performed and no grid search
+        inbetween, the best parameters will be returned from the
+        random search.
+
+        Returns:
+            dict | None: None if the random search was not performed,
+            else the best parameters as dictionary.
+        """
         if not self._random_search_performed:
             print('Random search not performed!')
             return None
         else:
             return self.predictor.model.best_params_
 
-    def save_random_search_results(
+    def _save_random_search_results(
             self, file_name: str = 'random_search_adjuvant_therapy'
     ) -> None:
+        """If random search was performed the best parameters will be saved
+        to a json file in the results directory with the file_name as
+        a json file.
+
+        Args:
+            file_name (str, optional): Name of the file without file extension.
+            Defaults to 'random_search_adjuvant_therapy'.
+        """
         best_params = self.get_best_random_search_parameters()
         if best_params is not None:
             save_path = self.predictor.args.results_dir / f'{file_name}.json'
@@ -85,13 +156,42 @@ class TabularAdjuvantTreatmentModelEvaluator:
 
     @staticmethod
     def create_hyper_parameter_space_random() -> dict[str, any]:
-        return {'Name': np.NAN}
+        """Should create a hyperparameter space for the base model.
+        Has to be implemented in the subclass.
+
+        Returns:
+            dict[str, any]: A dictionary with the hyperparameter names
+            as keys and the values as list of possible values.
+
+        Raises:
+            NotImplementedError: If the function is not implemented.
+        """
+        raise NotImplementedError('Hyperparameter Space Random not implemented!')
 
     @staticmethod
     def create_hyper_parameter_space_grid() -> dict[str, any]:
-        return {'Name': np.NAN}
+        """Should create a hyperparameter space for the base model.
+        Has to be implemented in the subclass.
+
+        Returns:
+            dict[str, any]: A dictionary with the hyperparameter names
+            as keys and the values as list of possible values.
+
+        Raises:
+            NotImplementedError: If the method is not implemented.
+        """
+        raise NotImplementedError('Hyperparameter Space Grid not implemented!')
 
     def _create_base_model(self) -> any:
+        """Should return the base model that is used for the evaluation.
+        Has to be implemented in the subclass.
+
+        Returns:
+            any: The base model that is used for the evaluation.
+
+        Raises:
+            NotImplementedError: If the method is not implemented.
+        """
         raise NotImplementedError('Base model not implemented!')
 
 
