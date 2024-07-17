cf28aa0baa18dfa6bcccf886942281a3626b1c98
dj-fau <david.julian.fischer@fau.de>
2024-07-17 13:43:13 +0200

    Restructured comparer class to include test performance and added docstrings

--- model_evaluation/tabular_adjuvant_therapy_model_comparison.py
+++ model_evaluation/tabular_adjuvant_therapy_model_comparison.py
@@ -5,7 +5,6 @@ from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
 sys.path.append(str(Path(__file__).parents[1]))
 from multimodal_machine_learning import TabularAdjuvantTreatmentPredictor, PredictionPlotter
@@ -107,6 +106,15 @@ class TabularAdjuvantTherapyComparer:
             self, save_flag: bool = False,
             plot_flag: bool = False, random_state: int = 42
     ):
+        """Creates a comparer for the different optimal models for the adjuvant therapy predictors.
+        To change the predictors that should be used, change the _predictors list together with the
+        _predictor_names and _predictor_short_names list.
+
+        Args:
+            save_flag (bool, optional): If the results should be saved. Defaults to False.
+            plot_flag (bool, optional): If the results should be plotted. Defaults to False
+            random_state (int, optional): The random state for the models. Defaults to 42.
+        """
         self._predictors = [
             OptimalRandomForestTabularAdjuvantTreatmentPredictor(
                 save_flag=False, plot_flag=False,
@@ -144,19 +152,45 @@ class TabularAdjuvantTherapyComparer:
         self._y_test = base_data[3]
 
     def compare_models(self):
+        """Function to compare the different models for the adjuvant therapy predictions.
+        """
         for predictor, name in zip(self._predictors, self._predictor_names):
             _ = predictor.train(
                 plot_name='comparison_adjuvant_treatment_multimodal'
             )
 
         average_adjuvant_treatment_predictions = self._random_data_evaluation()
-        training_data_performance = self._data_evaluation(
-            labels=self._y_train, data=self._x_train
+        training_data_performance, metric_names_train = self._data_evaluation(
+            labels=self._y_train, data=self._x_train, data_type='Train'
+        )
+        test_data_performance, metric_names_test = self._data_evaluation(
+            labels=self._y_test, data=self._x_test, data_type='Test'
         )
 
-        self._create_comparison(
-            average_random_data=average_adjuvant_treatment_predictions,
-            training_data_performance=training_data_performance
+        train_metrics = self._create_content_for_tabular_visualization(
+            average_random_data_performance=average_adjuvant_treatment_predictions,
+            data_performance=training_data_performance,
+            data_performance_metrics=metric_names_train,
+            data_type='Train'
+        )
+
+        test_metrics = self._create_content_for_tabular_visualization(
+            average_random_data_performance=average_adjuvant_treatment_predictions,
+            data_performance=test_data_performance,
+            data_performance_metrics=metric_names_test,
+            data_type='Test'
+        )
+
+        self._plotter.predictor_comparison_table(
+            metrics_data=train_metrics[1], row_labels=train_metrics[2],
+            metrics=train_metrics[0], fig_size=(20, 6),
+            plot_name='comparison_adjuvant_treatment_multimodal_train_only'
+        )
+
+        self._plotter.predictor_comparison_table(
+            metrics_data=test_metrics[1], row_labels=test_metrics[2],
+            metrics=test_metrics[0], fig_size=(20, 6),
+            plot_name='comparison_adjuvant_treatment_multimodal_test_only'
         )
 
     def _random_data_evaluation(self) -> list[np.ndarray]:
@@ -171,19 +205,23 @@ class TabularAdjuvantTherapyComparer:
         return average_predictions
 
     def _data_evaluation(
-            self, labels: np.array, data: np.ndarray | pd.DataFrame
-    ) -> list[list[int]]:
+            self, labels: np.array, data: np.ndarray | pd.DataFrame, data_type: str = 'Train'
+    ) -> tuple[list[list[int]], list]:
         """
         Compares the predictions from all predictors with the given labels for the data.
 
         Args:
             labels (np.array): The ground truth labels for the given data. Should be only 0's and
             1's
+
             data (np.ndarray | pd.DataFrame): The data for the given labels. Should be the same
             dimensionality as the data used to train the predictors.
 
+            data_type (str, optional): The type of the data only used for the metric_names.
+            Defaults to Train.
+
         Returns:
-            list[list[int]]: For each predictor a list will be returned with the following metrics
+            tuple[list[list[int]], list[str]]: For each predictor a list will be returned with the following metrics
             int that order [num_pos_pred, num_neg_pred, num_tp, num_fp, num_tn, num_fn,
             num_pos_as_predictor1, num_neg_as_predictor1, num_pos_as_predictor2, num_neg_as_predictor2,
             ...]
@@ -200,6 +238,8 @@ class TabularAdjuvantTherapyComparer:
 
             num_neg_as_predictorX (int): Number of negative predictions that were also done by
             predictor number X.
+
+            The second entry in the tuple is a list of the metric names.
         """
         y_predictions = []
         evaluation_results = []
@@ -237,35 +277,66 @@ class TabularAdjuvantTherapyComparer:
                 evaluation_results[index].append(num_also_pos)
                 evaluation_results[index].append(num_also_neg)
 
-        return evaluation_results
+        metrics_names = ['# pos. pred.', '# neg. pred.',
+                         f'TP {data_type}', f'FP {data_type}',
+                         f'TN {data_type}', f'FN {data_type}']
+        for short_name in self._predictor_short_names:
+            metrics_names += [f'# pos. as {short_name}', f'# neg. as {short_name}']
 
-    def _create_comparison(
-            self, average_random_data: list[np.ndarray],
-            training_data_performance: list[list[int]],
-    ) -> None:
-        if len(average_random_data) != len(training_data_performance) != len(self._predictors):
-            raise ValueError(
-                "The length of the given data does not match the number of predictors."
-            )
+        return evaluation_results, metrics_names
+
+    def _create_content_for_tabular_visualization(
+            self, average_random_data_performance: list[np.ndarray],
+            data_performance: list[list[int]],
+            data_performance_metrics: list[str], data_type: str = 'Train'
+    ) -> list:
+        """Creates the content that is necessary to display the evaluation results in
+        a table.
+
+        Args:
+            average_random_data_performance (list[np.ndarray]): For each predictor in self._predictors
+            the mean prediction on the random data.
+
+            data_performance (list[list[int]]): For each predictor in self._predictors the evaluation
+            results on the given data. Type of data should be indicated by data_type
+
+            data_performance_metrics (list[str]): The names of the metrics that are used for the evaluation.
+            Should have the same length as the data_performance for a single predictor.
+
+            data_type (str, optional): The name of the data used to create the data_performance.
+            Defaults to 'Train'.
+        """
+        metrics_names = [f'Average prediction random data'] + data_performance_metrics
+        metrics_data, row_labels = self._create_metric_data_and_row_labels_for_tabular_visualization(
+            data_performance=data_performance, average_random_data_performance=average_random_data_performance
+        )
+
+        return [metrics_names, metrics_data, row_labels]
+
+    def _create_metric_data_and_row_labels_for_tabular_visualization(
+            self, data_performance: list[list[int]], average_random_data_performance: list[np.ndarray]
+    ) -> tuple[list, list]:
+        """Creates a single list for the performance and also the row labels based on the
+        _predictor_names and _predictor_short_names.
+
+        Args:
+            data_performance (list[list[int]]): The performance of the predictors on the given data.
+
+            average_random_data_performance (list[np.ndarray]): The mean prediction for class 1 on the
+            random data.
+
+        Returns:
+            tuple[list, list]: The first list contains the performance data merged and the second list the
+            row_labels
+        """
         metrics_data = []
         row_labels = []
-
-        data_performance_metrics = ['# pos. pred', '# neg. pred', 'TP Train', 'FP Train', 'TN Train', 'FN Train']
-        for short_name in self._predictor_short_names:
-            data_performance_metrics += [f'# pos. as {short_name}', f'# neg. as {short_name}']
-        average_sign = "\u03BC"
-        metrics = [f'Average prediction random data'] + data_performance_metrics
-
         for average, data_performance, full_name, short_name in \
-                zip(average_random_data, training_data_performance, self._predictor_names, self._predictor_short_names):
+                zip(average_random_data_performance, data_performance,
+                    self._predictor_names, self._predictor_short_names):
             metrics_data.append([np.round(average, 2)] + data_performance)
             row_labels.append(f'{full_name} ({short_name})')
-
-        self._plotter.predictor_comparison_table(
-            metrics_data=metrics_data, row_labels=row_labels,
-            metrics=metrics, fig_size=(20, 6),
-            plot_name='comparison_adjuvant_treatment_multimodal_train_only'
-        )
+        return metrics_data, row_labels
 
 
 if __name__ == '__main__':
